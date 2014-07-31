31876fab1e7161a950720346be6f5b9b6595fe75
mnmnc <chooseright+git@gmail.com>
2014-07-31 15:33:03 +0200

    Added option parser, colors, organized procedures

--- src/panoptes.py
+++ src/panoptes.py
@@ -5,8 +5,33 @@ import os
 import hashlib
 import time
 import csv
+from optparse import OptionParser
+import argparse
 from functools import partial
 from colorama import init, Fore, Back, Style
+
+# VARIABLES
+
+paths = ["/bin", "/sbin"]
+outfile = "ids.db"
+newfile = "ids.tmp"
+fail = "["+ Fore.RED+Style.BRIGHT+" FAIL "+ Fore.RESET + Style.NORMAL + "]"
+succ = "["+ Fore.GREEN+Style.BRIGHT+" SUCC "+ Fore.RESET + Style.NORMAL + "]"
+info = "["+ Fore.CYAN+Style.BRIGHT+" INFO "+ Fore.RESET + Style.NORMAL + "]"
+warn = "["+ Fore.YELLOW+Style.DIM+" WARN "+ Fore.RESET + Style.NORMAL + "]"
+chce = "["+ Fore.YELLOW+Style.BRIGHT+" CHCE "+ Fore.RESET + Style.NORMAL + "]"
+fovr = "["+ Fore.WHITE+Style.BRIGHT+" FOVR "+ Fore.RESET + Style.NORMAL + "]"
+
+def nparse():
+	usage = " panoptes.py [-v | --verbose | -s | --silent | -p | --paths ]"
+	parser = argparse.ArgumentParser(description=usage)
+	parser.add_argument('-v', "--verbose", action='store_true', default=False, help='Show output for files that passed the integrity check.')
+	parser.add_argument('-d', "--details", action='store_true', default=False, help='Show detailed output for files that failed integrity check.')
+	parser.add_argument('-o', "--override", action='store_true', default=False, help='This forces the creation of new database if some files will generate new hash values.')
+	parser.add_argument('-p', "--path", metavar='additional_paths', default=[None], nargs='*', help='Include additional paths.')
+	args = parser.parse_args()
+	return args
+
 
 def fix( my_string, size ):
 	if len(my_string) > size:
@@ -32,79 +57,97 @@ def check_file_size(filename):
 		result = 0
 	return result
 
-outfile = "ids.db"
-newfile = "ids.tmp"
-of = open(newfile, 'w')
-be_silent_for_passed = 1
+def open_file(filename):
+	of = open(filename, 'w')
+	return of
 
-paths = ["/bin", "/sbin"]
+def create_index():
+	global args
+	
+	of = open_file(newfile)
 
-print("Creating current verification index.")
-for path in paths:
-	for root, dirs, files in os.walk(path):
-		for file in files:
-			f = root + "/" +file
-			#print(os.path.join(root, file), md5sum(os.path.join(root, file)), time.ctime(os.path.getmtime(f)) )
-			try:
-				#print( fix(os.path.join(root, file)), md5sum(os.path.join(root, file)), time.ctime(os.path.getmtime(f)), os.stat(f).st_size )
-				of.write( "\"" + os.path.join(root, file) + \
-					"\",\"" + md5sum(os.path.join(root, file)) + \
-					"\",\"" + time.ctime(os.path.getmtime(f)) + \
-					"\",\"" + str(os.stat(f).st_size) + "\"\n")
-			except:
-				pass
-			
-			#print( "last modified: %s" % time.ctime(os.path.getmtime(root,file)) )
+	if len(args.path) > 0:
+		for p in args.path:
+			if p != None:
+				paths.append(p)
 
-of.close()
-
-print(check_file_size(outfile))
-
-if check_file_size(outfile) == 0:
-	print("DB not found. Creating new one.")
-	of.close()
-	os.rename( newfile, outfile)
-else:
-	print("DB found. Comparison in progress.")
-	db_csv = csv.reader(open(outfile), delimiter=',', quotechar='"')
-	new_csv = csv.reader(open(newfile), delimiter=',', quotechar='"')
-	db_list = list(db_csv)
-	new_list = list(new_csv)
-
-	hash_error_count = 0
-
-	for row in db_list:
-		for inner in new_list:
-			if row[0] == inner[0]:
+	print( info + " Creating current verification index.")
+	for path in paths:
+		print( info + " Processing path ", path)
+		for root, dirs, files in os.walk(path):
+			for file in files:
+				f = root + "/" +file
 				
-				if row[1] == inner[1]:
-					if be_silent_for_passed == 0:
-						print( "Hash check for: ", fix(row[0], 30) , " Passed " )
+				try:
+					of.write( "\"" + os.path.join(root, file) + \
+						"\",\"" + md5sum(os.path.join(root, file)) + \
+						"\",\"" + time.ctime(os.path.getmtime(f)) + \
+						"\",\"" + str(os.stat(f).st_size) + "\"\n")
+				except:
+					pass
 
-				else:
-					hash_was = "Hash was: " + row[1]
-					hash_is = " Hash is: " + inner[1]
-					date_was = "Mod. date was: " + row[2]
-					date_is = " Mod. date is: " + inner[2]
-					size_was = "Size was: " + row[3]
-					size_is = " Size is: " + inner[3]
-					print("\nHash check for: ", fix(row[0], 30) ,  Fore.RED +  Style.BRIGHT + " Failed " + Fore.RESET + Style.NORMAL )
-					print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( hash_was, 40), fix(hash_is, 40) + Fore.RESET + Style.NORMAL )
-					print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( date_was, 40), fix(date_is, 40) + Fore.RESET + Style.NORMAL )
-					print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( size_was, 40), fix(size_is, 40) + Fore.RESET + Style.NORMAL )
+	of.close()
 
-					hash_error_count = hash_error_count + 1
+def validate_index():
+	global args
 
-	print(" ")
-	if hash_error_count != 0:
-		print(  Fore.RED +  Style.BRIGHT + "Error. Modifications detected." + Fore.RESET + Style.NORMAL )
-		override = input("Force update database? [y]es / [n]o: ")
-		if override == "y":
-			print("Overriding database with new one.")
-		else:
-			print("Override canceled.")
+	if check_file_size(outfile) == 0:
+		print( info + " DB not found. Creating new one.")
+		os.rename( newfile, outfile)
 	else:
-		print("System secure.")
-		os.rename( newfile, outfile )
+		print( info + " DB found. Comparison in progress.")
 
+		db_list = list(csv.reader(open(outfile), delimiter=',', quotechar='"'))
+		new_list = list(csv.reader(open(newfile), delimiter=',', quotechar='"'))
 
+		hash_error_count = 0
+
+		for row in db_list:
+			for inner in new_list:
+				if row[0] == inner[0]:
+					if row[1] == inner[1]:
+						if args.verbose == True:
+							print( succ + " Hash check for: ", fix(row[0], 30) )
+
+					else:
+						if args.details == True:
+							hash_was = "Hash was: " + row[1]
+							hash_is = " Hash is: " + inner[1]
+							date_was = "Mod. date was: " + row[2]
+							date_is = " Mod. date is: " + inner[2]
+							size_was = "Size was: " + row[3]
+							size_is = " Size is: " + inner[3]
+							print("\n" +fail+ " Hash check for: ", fix(row[0], 30)  )
+							print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( hash_was, 40), fix(hash_is, 40) + Fore.RESET + Style.NORMAL )
+							print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( date_was, 40), fix(date_is, 40) + Fore.RESET + Style.NORMAL )
+							print("\t\t", Fore.WHITE +  Style.BRIGHT + fix( size_was, 40), fix(size_is, 40) + Fore.RESET + Style.NORMAL )
+						else:
+							print( fail + " Hash check for: ", fix(row[0], 30) )
+
+						hash_error_count = hash_error_count + 1
+
+		print(" ")
+		if hash_error_count != 0:
+			print(  warn + Fore.YELLOW +  Style.DIM + " Modifications detected." + Fore.RESET + Style.NORMAL )
+			if args.override == True:
+				print( fovr + "Overriding database with new one.")
+				os.rename( newfile, outfile)
+			else:
+				override = input( chce + " Force update database? [y]es | [E]nter to cancel: ")
+				if override == "y":
+					print( info + " Overriding database with new one.")
+					os.rename( newfile, outfile)
+				else:
+					print( info + " Override canceled.")
+		else:
+			print( info + " System secure.")
+			os.rename( newfile, outfile )
+
+def main():
+	global args
+	args = nparse()
+	create_index()
+	validate_index()
+
+if __name__ == "__main__":
+	main()
